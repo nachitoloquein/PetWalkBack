fb26e69d3d93871c5f59dccaa0f38f0374316c55
nachito <elnachoescachondo@gmail.com>
2022-09-01 16:40:05 -0400

    avance de modelos y logica

--- src/controllers/trabajador.controller.js
+++ src/controllers/trabajador.controller.js
@@ -8,14 +8,14 @@ trabajadorCtrl.listarSolicitud = async(req,res)=>{
     res.json(solicitudes);
 }
 
-
 trabajadorCtrl.enviarSolicitud = async(req, res)=>{
 try{
-    const {nombre, apellido, comuna, solicitudPendiente, genero} = req.body;
-    const newSolicitud = new Trabajador({nombre, apellido, comuna, solicitudPendiente, genero});
+    const {nombre, apellido, comuna, genero, telefono, correo, contrasena, rut, direccion} = req.body;
+    const newSolicitud = new Trabajador({nombre, apellido, comuna, genero, telefono, correo, contrasena, rut, direccion});
     await newSolicitud.save();
 
     res.send({message: 'Solicitud creada'})
+    console.log(newSolicitud);
 }
 catch(err){
     res.send({message: err});
--- src/models/trabajador.model.js
+++ src/models/trabajador.model.js
@@ -5,8 +5,14 @@ const trabajadorSchema = new Schema({
     nombre: {type: String, required: true},
     apellido: {type: String, required: true},
     comuna: {type: String, required: true},
-    solicitudPendiente: {type: Boolean, required: true},
-    genero: {type: String, required: true}
+    solicitudPendiente: {type: Boolean, required: true, default: true},
+    genero: {type: String, required: true},
+    telefono: {type: String, required: true},
+    correo: {type: String, required: true},
+    contrasena: {type: String, required: true},
+    rut: {type: String, required:true},
+    direccion: {type: String, required: true},
+    activo: {type: Boolean, required: true, default: false}
 },{
     versionKey: false
 })
