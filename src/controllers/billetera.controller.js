c2af351c842879bfa796c006ecb699f34aac8ab4
luisRojasBorquez <lui.rojasb@alumnos.duoc.cl>
2022-10-31 10:28:55 -0300

    se crea metodo cargar coins
    Se creo metodo de cargar coins con id usuario y con id billetera en ambas deben enviar el monto de carga coins a través de un json y la id por url ya bien sea la id del consumidor o id billetera

--- src/controllers/billetera.controller.js
+++ src/controllers/billetera.controller.js
@@ -1,6 +1,7 @@
 CtrlBilletera = {}
 
-const Billetera = require('../models/billeteraConsumidor.model')
+const Billetera = require('../models/billeteraConsumidor.model');
+const { findById } = require('../models/plan.model');
 
 CtrlBilletera.crearBilletera = async(id)=>{
     const newBilletera = new Billetera({idConsumidor: id});
@@ -10,6 +11,29 @@ CtrlBilletera.crearBilletera = async(id)=>{
 CtrlBilletera.mostrarDatos = async(req, res)=>{
     const billetera = await Billetera.findOne({idConsumidor: req.params.id});
     res.send(billetera);
+}
+
+CtrlBilletera.cargarCoinsIdBill = async(req,res)=>{
+    try {
+        const consumidorCoins = await Billetera.findById(req.params.id);
+        const cargaCoins = consumidorCoins.monto + req.body.monto;
+        console.log(consumidorCoins)
+        await Billetera.findByIdAndUpdate(req.params.id,{$set: {monto:cargaCoins}});
+        res.status(200).send({status: 'Se cargaron coins correctamente'});
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+CtrlBilletera.cargarCoinsIdUsuario = async(req,res)=>{
+    try {
+        const consumidorCoins = await Billetera.find({idConsumidor:req.params.id});
+        const cargaCoins = consumidorCoins[0].monto + req.body.monto;
+        await Billetera.findOneAndUpdate({idConsumidor:req.params.id},{$set: {monto:cargaCoins}})
+        res.status(200).send({status: 'Se cargaron coins correctamente'});
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 module.exports = CtrlBilletera
--- src/routes/billetera.routes.js
+++ src/routes/billetera.routes.js
@@ -3,5 +3,8 @@ const billeteraCtrl = require('../controllers/billetera.controller');
 const router = Router();
 
 router.get('/:id', billeteraCtrl.mostrarDatos);
+router.put('/CargarCoinsIdBilletera/:id', billeteraCtrl.cargarCoinsIdBill)
+router.put('/CargarCoinsIdConsumidor/:id', billeteraCtrl.cargarCoinsIdUsuario)
+
 
 module.exports = router;
