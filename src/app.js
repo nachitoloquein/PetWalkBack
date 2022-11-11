1d3b40ff69adfefc8e92441f9b29408adae072cc
nachito <elnachoescachondo@gmail.com>
2022-11-11 14:00:37 -0300

    wsBoleta

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ app.use("/api/billetera",require('./routes/billetera.routes'));
 app.use("/api/webpay",require('./routes/webpay.routes'));
 app.use("/api/reporte",require('./routes/reporte.routes'));
 app.use("/api/match",require('./routes/match.routes'));
+app.use("/api/boleta",require('./routes/boleta.routes'));
 
 //Exportamos el archivo
 app.use('/files', express.static('files'));
